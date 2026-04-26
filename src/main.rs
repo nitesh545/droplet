3bf97f7cfe0aa47bbf98803de353f6a5a16427fb
nitesh545 <hitman12457868@gmail.com>
2026-04-26 21:56:51 +0530

    NIT-002 | setting up configs and postgresql pool

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 use axum::{Router, routing::get};
+use sqlx::{Pool, Postgres};
+use std::{
+    env,
+    sync::{Arc, Mutex},
+};
 use tower_http::trace::TraceLayer;
 use tracing::Level;
 
+#[allow(dead_code)]
+struct AppState {
+    pub db_pool: Pool<Postgres>,
+    pub config: Config,
+}
+
+#[allow(dead_code)]
+struct Config {
+    pub database_url: String,
+    pub port: String,
+}
+
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
+
+    let database_url = env::var("DATABASE_URL").expect("No DATABASE_URL");
+    let port = env::var("PORT").expect("No PORT");
+    let host = env::var("URL").expect("No URL");
+    let config = Config {
+        database_url: database_url.clone(),
+        port: port.clone(),
+    };
+
+    let pg_pool = Pool::<Postgres>::connect_lazy(&config.database_url)
+        .expect("could not connect with postgresql db");
+    let app_state = Arc::new(Mutex::new(AppState {
+        db_pool: pg_pool,
+        config,
+    }));
+
     let app = Router::new()
         .route("/hello", get(|| async { "hello world" }))
+        .with_state(app_state)
         .layer(TraceLayer::new_for_http());
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind(&format!("{:?}/hello", host))
         .await
         .unwrap();
     axum::serve(listener, app)
